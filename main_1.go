4ca61d96a58e8e32547a1ade4ae2b88a7f780bcf
HannibalLecktor <fans7288@gmail.com>
2017-12-05 19:52:27 +0500

    rabbit sender for test

---
+++ main_1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://fans:123456@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close();
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	body := "hello"
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing {
+			ContentType: "test/plain",
+			Body: []byte(body),
+		})
+
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+
+}
